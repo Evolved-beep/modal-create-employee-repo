1a11750ea489fd41444ddee1f7a92e9ed9a4db15
Evolved-beep <maxime.magnier@outlook.com>
2023-07-04 13:01:53 +0200

    Ajout de commentaire explicatif

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Modal from './Modal';
 
 function App() {
+    // This const will change the state of the modal 
     const [openModal, setOpenModal] = useState(true)
     return (
+        // We send the setter on the modal component for changed the state when you will close the modal
         openModal && <Modal closeModal={setOpenModal} />
     )
   
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -3,9 +3,11 @@ import './Modal.css'
 
 const Modal = ({closeModal}) => {
     return(
+        // ternaire condition for show the modal or not by switching the css class
         <div className={closeModal ? "modal_container" : "modal_display"}>
             <div className="modal_test">
                 <p>Employee created</p>
+                {/* the onClick even will change the modal state to false and remove the modal from the html */}
                 <div className="close_container">
                     <p onClick={() => closeModal(false)}>X</p>
                 </div>
